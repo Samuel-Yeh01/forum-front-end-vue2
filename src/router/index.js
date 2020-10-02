23795e4b97631e2e326141a0450a301f20e5b70e
Samuel_Yeh <samuel.yeh01@gmail.com>
2020-10-02 16:30:04 +0800

    create SignIn & SignUp page

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,21 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import NotFound from "../views/NotFound.vue";
+import SignIn from "../views/SignIn.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
+  {
+    path: "/signin",
+    name: "sign-in",
+    component: SignIn,
+  },
+  {
+    path: "/signup",
+    name: "sign-up",
+    component: () => import("../views/SignUp.vue"),
+  },
   {
     // 由於 Vue Router 路由會由上往下進行匹配，所以當路由一條條解析下來，所有的路由都無法匹配，最後就會匹配到 path: '*'，星號 (*) 是萬用字元，這裡代表「所有的網址」，也就是說不管使用者輸入什麼，只要找不到對應網頁，最後就會返回 NotFound 這個頁面。
     // 相反的，若使用者輸入的網址是 /about 時，因為由上往下解析時，跑到 path: '/about' 的地方就匹配成功了，因此就不會再往下。
