57c0d110bea9e49d1990a92fb87f886cef3447da
Samuel_Yeh <samuel.yeh01@gmail.com>
2020-10-07 17:57:08 +0800

    :sparkles: Introduce NavTabs with v-for & v-bind
    詳述：
    建立導覽頁籤元件：NavTabs
    
    備忘：
    在建立 v-for 時，採用uuidv4，拿來當成 key 屬性。

--- src/views/RestaurantsFeeds.vue
+++ src/views/RestaurantsFeeds.vue
@@ -1,8 +1,19 @@
 // 最新動態
 <template>
   <div class="container py-5">
+    <!-- 使用 NavTabs 元件 -->
+    <NavTabs />
     <h1 class="mt-5">
       最新動態
     </h1>
   </div>
 </template>
+
+<script>
+import NavTabs from "./../components/NavTabs";
+export default {
+  components: {
+    NavTabs,
+  },
+};
+</script>
